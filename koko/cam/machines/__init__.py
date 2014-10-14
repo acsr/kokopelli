f3afbb321ee41c8476a265d3e87e3ab3ae460c0d
Armin Stross-Radschinski <acsr.github@dev.acsr.de>
2014-10-14 11:56:14 +0200

    register machine file mimaki650

--- koko/cam/machines/__init__.py
+++ koko/cam/machines/__init__.py
@@ -5,6 +5,6 @@
 @details  Each input module must define NAME, INPUT, PANEL, and DEFAULTS (a list of tuples, each containing a default name and a dictionary which defines defaults for each potential panel).
 """
 
-import modela, epilog, universal, null, shopbot, gcode, shopbot5
+import mimaki650, modela, epilog, universal, null, shopbot, gcode, shopbot5
 
 MACHINES = [null, modela, epilog, universal, shopbot, gcode, shopbot5]
